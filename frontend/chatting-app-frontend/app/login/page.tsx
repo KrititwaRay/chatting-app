fd469b805c5b6a929a5bd23e7cb3a00eab2ffd28
krititwa <krititwaray97@gmail.com>
2026-07-21 22:03:43 +0530

    login page , verify page

--- frontend/chatting-app-frontend/app/login/page.tsx
+++ frontend/chatting-app-frontend/app/login/page.tsx
@@ -1,40 +1,85 @@
-import { Mail, ArrowRight } from 'lucide-react'
-import React from 'react'
+"use client"
+import axios from 'axios'
+import { Mail, ArrowRight, Loader2 } from 'lucide-react'
+import { useRouter } from 'next/navigation'
+import React, { useState } from 'react'
 
 const LoginPage = () => {
-  return (
-    <div className='min-h-screen bg-gray-900 flex items-center justify-center p-4'>
-        <div className='max-w-md w-full'>
-            <div className='bg-gray-800 border border-gray-700 rounded-lg p-8' >
 
-                <div className='text-center mb-8'>
-                    <div className='mx-auto w-20 h-20 bg-blue-600 rounded-lg flex items-center justify-center mb-6'>
-                        <Mail size={40} className='text-white'/>
-                    </div>
-                    <h1 className='text-4xl font-bold text-white mb-3'>
-                        Welcome To Chat App
-                    </h1>
-                    <p className='text-gray-300 text-ls'>
-                        Enter your email to continue your journey
-                    </p>
-                </div>
-                  <form className='space-y-6'>
-                      <div>
-                        <label htmlFor="email" className='text-sm font-medium text-gray-300 mb-2'>Email Address</label>
-                        <input type="email" id='email' className='w-full px-4 py-4 bg-gray-700 border border-gray-600 rounded-lg text-white placeholder-gray-400' placeholder='Enter your email address' required/>
-                      </div>
-                      <button type='submit' className='w-full bg-blue-600 text-white py-6 rounded-lg font-semibold hover:bg-blue-700 disabled:opacity-50 disabled:cursor-not-allowed'>
-                        <div className='flex items-center justify-center gap-2'>
-                            <span>Send Verification Code</span> <ArrowRight className='w-5 h-5'/>
+    const [email, setEmail] = useState<string>("")
+    const [loading, setLoading] = useState<boolean>(false)
+    const router = useRouter();
+
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>): Promise<void> => {
+    e.preventDefault();
+
+    setLoading(true);
+
+    try {
+        const { data } = await axios.post(
+            "http://localhost:5000/v1/user/login",
+            {
+                email,
+            }
+        );
+
+        console.log(data);
+      
+        alert(data.response.status.message);
+
+        router.push(`/verify?email=${email}`);
+
+    } catch (error: any) {
+        console.log(error);
+        alert(error.response?.data?.message || "Something went wrong");
+    } finally {
+        setLoading(false);
+    }
+};
+    return (
+        <div className='min-h-screen bg-gray-900 flex items-center justify-center p-4'>
+            <div className='max-w-md w-full'>
+                <div className='bg-gray-800 border border-gray-700 rounded-lg p-8' >
+
+                    <div className='text-center mb-8'>
+                        <div className='mx-auto w-20 h-20 bg-blue-600 rounded-lg flex items-center justify-center mb-6'>
+                            <Mail size={40} className='text-white' />
                         </div>
-                      </button>
-                  </form>
+                        <h1 className='text-4xl font-bold text-white mb-3'>
+                            Welcome To Chat App
+                        </h1>
+                        <p className='text-gray-300 text-ls'>
+                            Enter your email to continue your journey
+                        </p>
+                    </div>
+                    <form onSubmit={handleSubmit} className='space-y-6'>
+                        <div>
+                            <label htmlFor="email" className='text-sm font-medium text-gray-300 mb-2'>Email Address</label>
+                            <input type="email" id='email' className='w-full px-4 py-4 bg-gray-700 border border-gray-600 rounded-lg text-white placeholder-gray-400' placeholder='Enter your email address' value={email} required 
+                            onChange={e => setEmail(e.target.value)}
+                            />
+                        </div>
+                        <button type='submit' className='w-full bg-blue-600 text-white py-6 rounded-lg font-semibold hover:bg-blue-700 disabled:opacity-50 disabled:cursor-not-allowed' disabled={loading}>
+
+                            {
+                                loading ?( <div className='flex items-center justify-center gap-2'>
+                                
+                                <Loader2 className='w-5 h-5'/>
+                                Sending OTP to your mail...
+                            </div>)
+                                :
+                               ( <div className='flex items-center justify-center gap-2'>
+                                <span>Send Verification Code</span> <ArrowRight className='w-5 h-5' />
+                            </div>)
+                            }
+                        </button>
+                    </form>
+
+                </div>
 
             </div>
-
         </div>
-    </div>
-  )
+    )
 }
 
 export default LoginPage
